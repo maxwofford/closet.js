cd7cd1a56b86388f563e3cf9dbd459d26d1e0eee
Max Wofford <max@maxwofford.com>
2016-10-30 03:48:13 -0400

    Add canvas

--- scripts.js
+++ scripts.js
@@ -4,11 +4,19 @@ const dev = window.location.hostname === 'localhost'
 const uploader = document.querySelector('.uploader')
 const player = document.querySelector('.player')
 const slider = document.querySelector('.slider')
+const canvas = document.querySelector('.canvas')
+const ctx = canvas.getContext('2d')
 
-let duration = 0;
+let duration = 0
 
 slider.addEventListener('input', () => {
+  if(duration === 0)
+    return
   player.currentTime = slider.value * player.duration / 100
+  setTimeout(() => {
+    // This only works with a timeout of > ~50 for some reason
+    ctx.drawImage(player, 0, 0, canvas.width, canvas.height)
+  }, 50)
 })
 
 uploader.addEventListener('change', (event) => {
@@ -24,3 +32,4 @@ uploader.addEventListener('change', (event) => {
   player.src = fileURL
   duration = player.duration
 })
+
